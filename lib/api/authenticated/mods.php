75e1c483b9f664a3561e282594691e3337f3a1c1
Rennorb <18741506+SaculRennorb@users.noreply.github.com>
2025-05-29 02:32:09 +0200

    fixed double notification when a mod author was referenced in comments under their own mod

--- lib/api/authenticated/mods.php
+++ lib/api/authenticated/mods.php
@@ -37,22 +37,30 @@ switch($urlparts[1]) {
 				$commentId = $con->insert_ID();
 				$con->execute('update `mod` set comments = comments + 1 where assetid = ?', [$assetId]);
 
-				// user mentions
+				$creatorUserId = intval($modData['createdbyuserid']);
+				$currentUserId = intval($user['userid']);
+
+				// Notifications for user mentions:
 				if(preg_match_all('/user-hash="([a-z0-9]{20})"/i', $commentHtml, $rawMatches)) {
-					// @security: $rawMatches are validated to be alphanumeric and therefore sql inert by the regex. $commentId is known to be an integer
 					$foldedHashes = implode(',', array_map(fn($h) => "'$h'", $rawMatches[1]));
+					// The mod author always gets sent their own notification, but users thend to reply to them by @ing them.
+					// For this reason we take out any references to the mod author and ourself here (`user.userid not in ($creatorUserId, $currentUserId)`).
+
+					// @security: $rawMatches are validated to be alphanumeric and therefore sql inert by the regex. $commentId, $currentUserId and $creatorUserId are known to be integers.
 					$con->execute("
 						insert into notification (type, recordid, userid)
 						select 'mentioncomment', $commentId, userid
 						from user
-						where substring(sha2(concat(user.userid, user.created), 512), 1, 20) in ($foldedHashes)
+						where
+							substring(sha2(concat(user.userid, user.created), 512), 1, 20) in ($foldedHashes)
+							and user.userid not in ($creatorUserId, $currentUserId)
 					");
 				}
 
-				// send notification about the new comment to the main mod author
+				// Send notification about the new comment to the main mod author:
 				//TODO(Rennorb): Send notifications to all opt-in contributors, requires adding config option and table changes.
-				$creatorUserId = intval($modData['createdbyuserid']);
-				if($user['userid'] != $creatorUserId) {
+				if($currentUserId !== $creatorUserId) { // Don't send a notification to ourself if we are the one commenting.
+					// @security: $commentId and $creatorUserId are known to be integers.
 					$con->execute("insert into notification (type, recordid, userid) values ('newcomment', $commentId, $creatorUserId)");
 				}
 
